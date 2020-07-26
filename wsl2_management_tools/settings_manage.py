67c67c86ce2d76ece1df77e7b074b20595cbbadc
IT小强xqitw.cn <mail@xqitw.cn>
2020-07-26 13:29:02 +0800

    配置操作类

---
+++ wsl2_management_tools/settings_manage.py
@@ -0,0 +1,79 @@
+# ==================================================================
+#       文 件 名: settings_manage.py
+#       概    要: 配置操作类，获取、保存配置
+#       作    者: IT小强 
+#       创建时间: 2019/12/24 18:14
+#       修改时间: 
+#       copyright (c) 2016 - 2019 mail@xqitw.cn
+# ==================================================================
+
+import os
+from os.path import join, isfile
+from json import loads, dumps
+
+from wsl2_management_tools.util import create_settings_path
+
+
+class SettingsManage:
+    """
+    配置操作类，获取、保存配置
+    """
+
+    # 默认配置数据
+    __settings = {
+        'SERVER_PORT': 9205,
+    }
+
+    def __init__(self, base_dir: str):
+        """
+        初始化
+        """
+
+        self.settings_dir = create_settings_path(path_name=os.path.join(base_dir, '.wsl2_management_tools'))
+        self.settings_file = join(self.settings_dir, 'settings.json')
+        self.__get_file_content()
+
+    def set(self, name, value):
+        """
+        设置配置
+        @param name: 配置名称
+        @param value: 配置值
+        """
+        self.__settings[name] = value
+        self.save_file_content(self.settings_file, dumps(self.__settings))
+
+    def get(self, name=None, default_value=None):
+        """
+        获取配置
+        @param name: 配置名称 None 会返回全部配置
+        @param default_value: 不存在时返回的默认值
+        @return: 返回配置值
+        """
+        if not name:
+            return self.__settings
+        return self.__settings.get(name, default_value)
+
+    def __get_file_content(self):
+        """
+        读取json文件的内容并转为字典
+        """
+
+        # 如果文件不存在则创建文件
+        if not isfile(self.settings_file):
+            self.save_file_content(self.settings_file, dumps(self.__settings))
+
+        # 读取文件内容
+        with open(self.settings_file, 'r') as f:
+            content = f.read()
+
+        # 转为json
+        self.__settings.update(loads(content))
+
+    @staticmethod
+    def save_file_content(file: str, content: str):
+        """
+        保存配置到json文件
+        """
+
+        with open(file, 'w') as f:
+            f.write(content)
